86149158f3d187d85274fe987e3f094de5786764
dnldl <dnldlscjf@gmail.com>
2024-02-27 03:04:35 +0900

    Exception 과 Service 설정 완료 아직 Repository 설정라지 않아서 에러뜸

--- src/main/java/net/javaguides/springmvc/service/CustomerService.java
+++ src/main/java/net/javaguides/springmvc/service/CustomerService.java
@@ -1,4 +1,17 @@
 package net.javaguides.springmvc.service;
 
-public class CustomerService {
+import net.javaguides.springmvc.entity.Customer;
+import net.javaguides.springmvc.exception.ResourceNotFoundException;
+
+import java.util.List;
+
+public interface CustomerService {
+
+    public List < Customer > getCustomers();
+
+    public void saveCustomer(Customer theCustomer);
+
+    public Customer getCustomer(int theId) throws ResourceNotFoundException;
+
+    public void deleteCustomer(int theId) throws ResourceNotFoundException;
 }
