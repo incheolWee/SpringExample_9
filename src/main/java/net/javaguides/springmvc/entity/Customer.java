2cbc812bbb2b5275eabaf7ee71c60e5215ea0022
dnldl <dnldlscjf@gmail.com>
2024-02-28 03:24:12 +0900

    코드 쪼개기 comment

--- src/main/java/net/javaguides/springmvc/entity/Customer.java
+++ src/main/java/net/javaguides/springmvc/entity/Customer.java
@@ -1,18 +1,18 @@
 package net.javaguides.springmvc.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import lombok.Data;
+
+import javax.persistence.*;
 
 @Entity
-@Table(name = "customer")
+@Data // lombok 사용해서  setter, getter toString 생성자 자동 등록
+@Table(name = "customer")  // customer 이라는 table에 매핑해준다
 public class Customer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  //기본키를 자동으로 생성해주는 어노테이션
+    //IDENTITY는 기본 키생성을 데이터베이스에 위임하는 전략이다. id값을 따로 할당하지 않아도 db가 자동으로 auto_increment를 해서 기본키 생성 주로 MySQL, PostgreSQL, SQL Server에서 사용한다.
+
     @Column(name = "id")
     private int id;
 
@@ -25,44 +25,5 @@ public class Customer {
     @Column(name = "email")
     private String email;
 
-    public Customer() {
 
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public String toString() {
-        return "Customer [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
-    }
 }
